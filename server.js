e0e4813b1e27d81e23bc2dd36b2868fcce2bce96
davidmendezaccenture <david.a.mendez@fundaula2gcp.com>
2025-06-13 09:48:55 +0000

    dockerfile modified port changed

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.get('/', (req, res) => {
 });
 
 // Puerto
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
