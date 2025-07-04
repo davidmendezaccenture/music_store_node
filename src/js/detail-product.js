357861751abd126c381f898425b7ad05cde9341f
Vicente Martin Lara <vicentemartin.dev@gmail.com>
2025-07-04 08:44:00 +0200

    estilos_visuales_registro_login

---
+++ src/js/detail-product.js
@@ -0,0 +1,35 @@
+// Función para obtener parámetros de la URL
+function getQueryParam(param) {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get(param);
+}
+
+// Cargar producto y mostrarlo
+$(document).ready(function () {
+    const productId = getQueryParam("id");
+    if (!productId) return;
+
+    $.getJSON("../assets/data/products.json", function (productos) {
+        const producto = productos.find(
+            (p) => String(p.id) === String(productId)
+        );
+        if (!producto) return;
+
+        // Actualiza los elementos del DOM con los datos del producto
+        $(".product-image")
+            .attr("src", producto.image)
+            .attr("alt", producto.name);
+        $(".product-category").text(producto.category);
+        $(".product-name").text(producto.name);
+        $(".product-price").text(
+            `€${producto.offerPrice < producto.price
+                ? producto.offerPrice
+                : producto.price
+            }`
+        );
+        $(".product-old-price").text(
+            producto.offerPrice < producto.price ? `€${producto.price}` : ""
+        );
+        $(".product-description").text(producto.description);
+    });
+});
