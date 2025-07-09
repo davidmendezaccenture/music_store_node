f884914c532924aa24bd5dd3558afc90d57f19b0
Lorena-Rodriguez <lorenarodriguezc@gmail.com>
2025-07-09 14:36:39 +0200

    arreglo funcionalidad de búsqueda global

--- src/js/app.js
+++ src/js/app.js
@@ -23,21 +23,7 @@ $(document).ready(function () {
       }
     });
 
-    // Asocia aquí el evento de búsqueda global
-    $(document).on("input", "#search-input-global", function () {
-      $("#globalSearchDropdown").show();
-      const query = $(this).val().trim().toLowerCase();
-      let resultados = [];
-      if (query.length > 1) {
-        const queryNorm = normalizeText(query);
-        resultados = allProducts.filter(
-          (p) =>
-            normalizeText(p.name).includes(query) ||
-            (p.category && normalizeText(p.category).includes(query))
-        );
-      }
-      renderGlobalResults(resultados, query);
-    });
+
     // Estado inicial del botón
     if (localStorage.getItem("darkMode") === "enabled") {
       $("body").addClass("dark-mode");
